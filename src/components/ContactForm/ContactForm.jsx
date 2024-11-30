b12367ff98dbaf537b3c7b2053d0c399e33f9b95
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-11-30 19:14:35 +0200

    fix formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addContact, selectContacts } from "../../redux/contactsSlice";
-import { ErrorMessage, Form, Formik } from "formik";
+import { ErrorMessage, Form, Field, Formik } from "formik";
+import * as Yup from "yup";
 import { MdPersonAddAlt1 } from "react-icons/md";
 import s from "./ContactForm.module.css";
 
-const ContactsForm = () => {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
+const FeedbackSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long")
+    .required("Required"),
+});
+
+const ContactForm = () => {
   const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (values, { resetForm }) => {
+    const { name, number } = values;
 
     if (contacts.some((contact) => contact.name === name)) {
       alert(`${name} is already in contacts.`);
@@ -20,34 +30,33 @@ const ContactsForm = () => {
     }
 
     dispatch(addContact({ id: Date.now(), name, number }));
-    setName("");
-    setNumber("");
+    resetForm();
   };
 
   return (
-    <Formik>
-      <Form className={s.form} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      validationSchema={FeedbackSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={s.form}>
         <div className={s.inputBox}>
-          <input
+          <Field
             className={s.input}
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name="name"
             placeholder="Name"
-            required
           />
-          <ErrorMessage name="name" component="span" />
+          <ErrorMessage name="name" component="span" className={s.error} />
         </div>
         <div className={s.inputBox}>
-          <input
+          <Field
             className={s.input}
             type="tel"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
+            name="number"
             placeholder="Number"
-            required
           />
-          <ErrorMessage name="number" component="span" />
+          <ErrorMessage name="number" component="span" className={s.error} />
         </div>
         <button className={s.btn} type="submit">
           <MdPersonAddAlt1 />
@@ -58,4 +67,4 @@ const ContactsForm = () => {
   );
 };
 
-export default ContactsForm;
+export default ContactForm;
